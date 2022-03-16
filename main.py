cd6990a5b8b4c19d4b31f7570a0c8a82b2dd0bea
m6hmd <85321205+m6hmd@users.noreply.github.com>
2022-03-16 04:54:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,38 +6,37 @@ import logging
 from config import *
 from flask import Flask, request
 
-BOT_TOKEN =  "1132931143:AAGwzukJusNzc5XjSvIMS92X8qLtGo9_Im4"
+BOT_TOKEN = "1132931143:AAGwzukJusNzc5XjSvIMS92X8qLtGo9_Im4"
 bot = telebot.TeleBot(BOT_TOKEN)
 server = Flask(__name__)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
+
+
 @bot.message_handler(content_types=['text'])
 def getID(message):
-    numCH = 0
-    pastebin = (message.text.split("|")[0])
-    num = int(message.text.split("|")[1])
+    check = 0
+    more = 0
+    less = 0
+    pastebin = message.text.
+
     idS = requests.get(str(pastebin)).text.splitlines()
-    
-    sendM = bot.send_message(message.chat.id, f'-ID : 000000000\nCHECK : {str(numCH)}\nPastebin : {pastebin}')
-    while True:
-        fil = idS[num]
-        bot.edit_message_text(chat_id=message.chat.id, message_id=sendM.message_id,text= f'- ID : {fil}\nCHECK : {str(numCH)}\nPastebin : {pastebin}')
-        url = "http://35.181.7.112/check.php?oid={}&submit=submit".format(fil)
-   
-        smahi = str(requests.get(url=url).text)
-        if 'coins":"' in smahi:
-            coin = str(smahi.split('coins":"')[1])
-            coins = str(coin.split('"')[0])
-            requests.get(f'''https://api.telegram.org/bot1111603340:AAHuKvMBE6z5Sk1rQ_jl-yWi12UVcjBU91U/sendMessage?chat_id=420953620&text=✅ Have coins {coins} ==> {fil}''')
-            numCH +=1
-            num +=1
-            if int(coins) > 800 or int(coins) == 800:
-                requests.get( f'''https://api.telegram.org/bot1166194091:AAEEDj7xjmgTt3ipvXTycCK44280OJflUaA/sendMessage?chat_id=420953620&text=✅ Have coins {coins} ==> {fil}''')
-                if int(coins) > 2000 or int(coins) == 2000:
-                    requests.get( f'''https://api.telegram.org/bot5208965780:AAHhLQrfv_7-LybUzRxt3_heQV13JocX0eY/sendMessage?chat_id=420953620&text=✅ Have coins {coins} ==> {fil}''')
+
+    sendM = bot.send_message(message.chat.id, f'-ID : 000000000\nCHECK : {str(check)}\nPastebin : {pastebin}')
+    for fil in idS:
+        bot.edit_message_text(chat_id=message.chat.id, message_id=sendM.message_id,
+                              text=f'- ID : {fil}\nCHECK : {str(check)}\nMore 800 : {str(more)}\nLess 800 : {str(less)}\nPastebin : {pastebin}')
+        url = f"https://fluidhighserver.mohammedhaiderz.repl.co/?id={fil}"
+        chk = requests.get(url).text
+        if '"successful+800"' in chk:
+            more += 1
+        elif '"successful-800"' in chk:
+            less += 1
+        elif '"not coin"' in chk:
+            pass
         else:
-            num += 1
-            numCH += 1
+            pass
+        check += 1
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
